8e7dd3163542483f7e0bedf3e20f2c2fc6ad84f6
Robert MacGregor <ragoradev@gmail.com>
2021-05-27 12:44:39 -0400

    * BugFix: Commit nativefunction.cpp.

---
+++ source/nativefunction.cpp
@@ -0,0 +1,31 @@
+/**
+ *  Copyright 2021 Robert MacGregor
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <torquescript/nativefunction.hpp>
+#include <torquescript/instructions/instruction.hpp>
+#include <torquescript/stringhelpers.hpp>
+#include <torquescript/storedvaluestack.hpp>
+
+namespace TorqueScript
+{
+    NativeFunction::NativeFunction(NativeFunctionPointer native, const std::string& package, const std::string& space, const std::string& name) : mNativeFunction(native), Function(package, space, name)
+    {
+
+    }
+
+    void NativeFunction::execute(std::shared_ptr<ConsoleObject> thisObject, std::shared_ptr<ExecutionState> state, const unsigned int argumentCount)
+    {
+        this->mNativeFunction(thisObject, state, argumentCount);
+    }
+}
