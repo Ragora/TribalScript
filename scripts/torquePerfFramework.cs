cac35c7ea239fc8b4be984535af5da5908be5930
Robert MacGregor <ragoradev@gmail.com>
2021-07-23 09:08:58 -0400

    * Tool: Add torquePerformance Framework under scripts.

---
+++ scripts/torquePerfFramework.cs
@@ -0,0 +1,145 @@
+function localVariableBenchMark(%samples)
+{
+	echo("localVariableBenchMark: Begin");
+
+	%result = "";
+	for (%sample = 0; %sample < %samples; %sample++)
+	{
+		%start = getRealTime();
+		for (%iteration = 0; %iteration < 2000000; %iteration++)
+		{
+		}
+		%delta = getRealTime() - %start;
+		
+		if (%result $= "")
+		{
+			%result = %delta;
+		}
+		else
+		{
+			%result = %result @ "," @ %delta;
+		}
+	}
+	
+	echo("localVariableBenchMark: End");
+	return %result;
+}
+
+function benchFunction() 
+{
+}
+
+function functionCallOverhead(%samples)
+{
+	echo("functionCallOverhead: Begin");
+	
+	%result = "";
+	for (%sample = 0; %sample < %samples; %sample++)
+	{
+		%start = getRealTime();
+		for (%iteration = 0; %iteration < 2000000; %iteration++)
+		{
+			benchFunction();
+		}
+		%delta = getRealTime() - %start;
+		
+		if (%result $= "")
+		{
+			%result = %delta;
+		}
+		else
+		{
+			%result = %result @ "," @ %delta;
+		}
+	}
+
+	echo("functionCallOverhead: End");
+	return %result;
+}
+
+function benchFunctionThreeParameter(%a, %b, %c) 
+{
+}
+
+function functionCallThreeParameterOverhead(%samples)
+{
+	echo("functionCallThreeParameterOverhead: Begin");
+	
+	%result = "";
+	for (%sample = 0; %sample < %samples; %sample++)
+	{
+		%start = getRealTime();
+		for (%iteration = 0; %iteration < 2000000; %iteration++)
+		{
+			benchFunctionThreeParameter(1, 2, 3);
+		}
+		%delta = getRealTime() - %start;
+		
+		if (%result $= "")
+		{
+			%result = %delta;
+		}
+		else
+		{
+			%result = %result @ "," @ %delta;
+		}
+	}
+
+	echo("functionCallThreeParameterOverhead: End");
+	return %result;
+}
+
+function objectOverhead(%samples)
+{
+	echo("objectOverhead: Begin");
+	
+	%result = "";
+	for (%sample = 0; %sample < %samples; %sample++)
+	{
+		%start = getRealTime();
+		for (%iteration = 0; %iteration < 2000000; %iteration++)
+		{
+			%object = new FileObject();
+			%object.delete();
+		}
+		%delta = getRealTime() - %start;
+		
+		if (%result $= "")
+		{
+			%result = %delta;
+		}
+		else
+		{
+			%result = %result @ "," @ %delta;
+		}
+	}
+
+	echo("objectOverhead: End");
+	return %result;
+}
+
+// Run all tests
+%sampleCount = 10;
+echo("Running benchmarks ...");
+%localVariableResult = localVariableBenchMark(%sampleCount);
+%functionCallOverheadResult = functionCallOverhead(%sampleCount);
+%functionCallThreeParameterOverheadResult = functionCallThreeParameterOverhead(%sampleCount);
+%objectOverheadResult = objectOverhead(%sampleCount);
+
+// Output to disk
+%handle = new FileObject();
+%handle.openForWrite("result.csv");
+%handle.write("" NL "");
+%handle.write("" NL "");
+
+// Write results
+%handle.write("Local Variable," @ %localVariableResult NL "");
+%handle.write("Function Call Overhead," @ %functionCallOverheadResult NL "");
+%handle.write("Function Call Three Parameter Overhead," @ %functionCallThreeParameterOverheadResult NL "");
+%handle.write("Object Overhead," @ %objectOverheadResult NL "");
+
+// Finish write
+%handle.close();
+%handle.delete();
+
+echo("Benchmark results Written to: result.csv");
